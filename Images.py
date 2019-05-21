7707a6e09173100a162b1e8dbe0a4c1e09f6b9f0
schmidtt <schmidtt@berea.edu>
2019-05-21 09:17:03 -0400

    Cleaned up some things

--- Images.py
+++ Images.py
@@ -1,6 +1,36 @@
-class Image:
+class Images:
     def __init__(self):
         self.file_name = ""
         self.caption_link = ""
         self.transcription = ""
         self.page_source = ""
+        self.image_count = 0
+        self.image_dict = {}
+
+    def populate(self):
+        """
+        This function populates the internal dictionary for storing all images, with the current info stored in the
+        self variables
+        :return: none
+        """
+        self.image_dict[self.file_name] = {"Filename": self.file_name,
+                                           "Caption Link": self.caption_link,
+                                           "Transcription": self.transcription,
+                                           "Page Source": self.page_source}
+        self.image_count += 1
+        self.file_name = ""
+        self.caption_link = ""
+        self.transcription = ""
+        self.page_source = ""
+
+    def list_images(self):
+        """
+        This is to list out all of the images currently in the image_dict variable
+        :return: None
+        """
+        for image in self.image_dict.keys():
+            to_print = image + ":\n"
+            for info in self.image_dict[image].keys():
+                to_print += info + ": " + self.image_dict[image][info] + "\n"
+            print(to_print)
+            print()
--- Scraper.py
+++ Scraper.py
@@ -1,19 +1,63 @@
 from bs4 import BeautifulSoup
-from Images import Image
+from Images import Images
+
+
+def caption_link(attribute, Images):
+    """
+    This function takes a given attribute, checks to see if it is the attribute for linking images with captions, and
+    then stores that information in the instance of the Images class
+    :param attribute: a string for an attribute of an html tag
+    :param Images: an instance of the Images class for storing the given  information
+    :return: None
+    """
+    if "aria-describedby" in attribute:
+        Images.caption_link = attribute[18:-1]
+
+
+def image_file_name(attribute, Images):
+    """
+    This function is for extracting the filename of an image from it's src filepath
+    :param attribute: a string for an attribute of an html tag
+    :param Images: an instance of the Images class for storing the given  information
+    :return: None
+    """
+    if "src" in attribute:
+        ext = attribute[-5:-1]  # the image's file extension to be added after the filename is retrieved
+        slash_total = attribute.count("/")
+        slash_count = 0
+        in_filename = False  # a check so we only append characters related to the filename (ignoring filepath)
+        for char in attribute:
+            if char == "/":
+                slash_count += 1
+            if slash_count == slash_total:  # this indicates the end of the filepath meaning the following will be the filename
+                in_filename = True
+            if char == "-":  # This is followed by ###x### indicating the scaled resolution which we don't want
+                in_filename = False  # This is so we stop adding characters and avoid the resolution information
+            if in_filename:
+                Images.file_name += char
+
+        Images.file_name += ext  # once the file name is obtained, append the file's extension
+        print(Images.file_name)
 
 
 def main():
+    # path = input("Please enter the file path to the directory where the html files are stored: ")
     path = "/home/schmidtt/PycharmProjects/PMSS_Scraper/html/"
     f = open(path + "EVELYN K. WELLS - PINE MOUNTAIN SETTLEMENT SCHOOL COLLECTIONS.html")
     web_page = BeautifulSoup(f, 'html.parser')
+    pmss_images = Images()
     for image in web_page.find_all('img'):
-        temp = Image()
         for attribute in str(image).split():
-            if "aria-describedby" in attribute:
-                temp.caption_link = attribute[18:-1]
-                print(temp.caption_link)
-        # print(str(image).split())
-        # print(image)
+
+            # this is for pulling out the id that will be used for linking images and captions
+            caption_link(attribute, pmss_images)
+
+            # this is to pull the filename out of the html
+            image_file_name(attribute, pmss_images)
+
+            # Using the temporary variable's information to copy over to a proper variable
+        pmss_images.populate()
+    pmss_images.list_images()
 
 
 if __name__ == "__main__":
