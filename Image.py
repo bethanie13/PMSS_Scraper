f7e87b6504f703ca658cc95ddaf67c45ddd8747c
schmidtt <schmidtt@berea.edu>
2019-05-24 09:33:46 -0400

    Cleaned up the image info functions and linked images with captions

--- Image.py
+++ Image.py
@@ -8,6 +8,34 @@ class Image:
         self.caption = ""
         self.image_resized_resolution = [0, 0]
 
+    def strip_resolution(self):
+        """
+        Uses the resolution information to clean up the filename
+        :param self: An Image object
+        :return: None
+        """
+        resolution = str(self.image_resized_resolution[0]) + "x" + str(self.image_resized_resolution[1])
+
+        if self.file_name.count(resolution) == 0:
+            return
+        else:
+            final_file_pieces = []
+            ext = self.file_name[-4:]  # Assuming the extension is 3 characters long save the last few characters
+            if "." not in ext:  # If the "." is not in the extension, the extension is 4 characters long
+                ext = self.file_name[-5:]
+            self.file_name = self.file_name[:-len(ext)]
+            split_name = self.file_name.split("-")
+            for piece in split_name:
+                if piece != resolution:
+                    final_file_pieces.append(piece)
+        final_file = ""
+        for i in range(len(final_file_pieces)):
+            if i == len(final_file_pieces)-1:
+                final_file += final_file_pieces[i]
+            else:
+                final_file += final_file_pieces[i] + "-"
+        self.file_name = final_file + ext
+
     def list_images(self):
         """
         This is to list out all of the information for this image
@@ -17,6 +45,7 @@ class Image:
         print("Transcription: " + self.transcription)
         print("Upload Date: " + self.upload_date)
         print("Resolution: " + str(self.image_resized_resolution[0]) + "x" + str(self.image_resized_resolution[1]))
-        print("Caption: " + self.caption)
+        if self.caption != "":
+            print("Caption: " + self.caption)
 
 
--- Scraper.py
+++ Scraper.py
@@ -1,104 +1,175 @@
 from bs4 import BeautifulSoup
-from Images import Image
+from Image import Image
+from Caption import Caption
 import copy
 import csv
+from os import listdir
+from os.path import isfile, join
 
 
-def caption_link(attribute, Image):
+def image_info(page_soup):
+    """
+    Finds all images on the page and collects as much information as it can
+    :param page_soup: A Beautiful Soup object
+    :return: A dictionary containing all of the images from the page
+    """
+    images_dict = {}
+    for image in page_soup.find_all('img'):
+        temp = Image()
+
+        # Pull out the id that will be used to find this image's caption
+        caption_link(image, temp)
+
+        # Pull the filename and upload date for this image
+        image_file_path_info(image, temp)
+
+        # Record the resolution information for this image
+        image_resolution(image, temp)
+
+        # If resolution information is in the filename, strip that information out
+        temp.strip_resolution()
+
+        if temp.file_name != "cropped-pmss_spelman_pntg_edited_2_brightened_x.jpg":  # Ignore the header image
+            images_dict[temp.file_name] = copy.copy(temp)  # Copy the image to a dictionary
+    return images_dict
+
+
+def caption_link(tag, img):
     """
     This function takes a given attribute, checks to see if it is the attribute for linking images with captions, and
     then stores that information in the instance of the Images class
-    :param attribute: a string for an attribute of an html tag
-    :param Image: an instance of the Images class for storing the given  information
+    :param tag: a string for an attribute of an html tag
+    :param img: an instance of the Images class for storing the given  information
     :return: None
     """
-    if "aria-describedby" in attribute:
-        Image.caption_link = attribute[18:-1]
+    img.caption_link = tag.get("aria-describedby")
 
 
-def image_file_path_info(attribute, Image):
+def image_file_path_info(tag, img):
     """
     This function is for extracting the filename of an image from it's src filepath. In addition, we can extract the
     upload date
-    :param attribute: a string for an attribute of an html tag
-    :param Image: an instance of the Images class for storing the given  information
+    :param tag: a string for an attribute of an html tag
+    :param img: an instance of the Images class for storing the given  information
     :return: None
     """
-    if "src=" in attribute:
-        ext = attribute[-5:-1]  # Assuming the extension is 3 characters long save the last few characters
-        if "." not in ext:  # If the "." is not in the extension, the extension is 4 characters long
-            ext = attribute[-6:-1]
-        slash_total = attribute.count("/")
-        slash_count = 0
-
-        for char in attribute:
-            # A slash count is used to know where in the file path we are. When count == total, we are in the file name
-            if slash_total == slash_count:
-                Image.file_name += char
-            if slash_total-2 <= slash_count < slash_total:
-                Image.upload_date += char
-            if char == "/":
-                slash_count += 1
-        Image.upload_date = Image.upload_date[:-1]
-        hyphen_total = Image.file_name.count("-")
-        hyphen_count = 0
-        final_file = ""
-        for char in Image.file_name:
-            if char == "-":
-                hyphen_count += 1
-            if not hyphen_count == hyphen_total:
-                final_file += char
-
-        Image.file_name = final_file
-        Image.file_name += ext  # once the file name is obtained, append the file's extension
+    file_path = tag.get("src")
+    file_split = file_path.split("/")
+    img.file_name = file_split[-1]
+    year = file_split[-3]
+    month = file_split[-2]
+    img.upload_date = month + "/" + year
 
 
-def image_resolution(attribute, Image):
-    if "width=" in attribute:
-        Image.image_resized_resolution[0] = attribute[7:-3]
-    if "height=" in attribute:
-        Image.image_resized_resolution[1] = attribute[8:-1]
+def image_resolution(tag, img):
+    """
+    Records the resolution of the image that's displayed
+    :param tag: A Beautiful Soup Tag object
+    :param img: An Image object
+    :return: None
+    """
+    img.image_resized_resolution[0] = tag.get("width")
+    img.image_resized_resolution[1] = tag.get("height")
 
 
-def write_csv(dict):
-    with open('images.csv', 'w') as csvfile:
-        filewriter = csv.writer(csvfile)
+def find_captions(page_soup):
+    """
+    Finds all text that might be a caption (we don't know until we compare the id attribute with the images
+    aria-describedby attribute)
+    :param page_soup: Beautiful Soup object
+    :return: None
+    """
+    captions_dict = {}
+    for caption in page_soup.find_all('dd'):
+        temp = Caption()
+        temp.image_link = caption.get('id')
+        temp.caption = caption.string[5:-5]
+        captions_dict[temp.image_link] = copy.copy(temp)
+    for caption in page_soup.find_all('p'):
+        temp = Caption()
+        temp.image_link = caption.get('id')
+        temp.caption = caption.string
+        captions_dict[temp.image_link] = copy.copy(temp)
+    return captions_dict
+
+
+def image_caption_linking(captions_dict, images_dict):
+    """
+    Matches image objects with their captions
+    :param captions_dict: Dictionary holding all captions from the page
+    :param images_dict: Dictionary holding all images from the page
+    :return: None
+    """
+    for caption in captions_dict.keys():
+        for image in images_dict.keys():
+            if captions_dict[caption].image_link == images_dict[image].caption_link:
+                images_dict[image].caption = captions_dict[caption].caption
+
+
+def write_csv(dict_to_write, csv):
+    """
+    Uses the csv library to write .csv files containing the image's information
+    :param dict_to_write:
+    :return:
+    """
+    with open(csv, 'w') as csvfile:
+        file_writer = csv.writer(csvfile)
         csv_data = [["Filename", "Transcription", "Upload Date", "Resolution"]]
-        for image in dict.keys():
-            csv_data.append([dict[image].file_name, dict[image].transcription,
-                             dict[image].upload_date, dict[image].image_resized_resolution[0] + "x" +
-                             dict[image].image_resized_resolution[1]])
-        filewriter.writerows(csv_data)
+        for image in dict_to_write.keys():
+            csv_data.append([dict_to_write[image].file_name, dict_to_write[image].transcription,
+                             dict_to_write[image].upload_date, dict_to_write[image].image_resized_resolution[0] + "x" +
+                             dict_to_write[image].image_resized_resolution[1]])
+        file_writer.writerows(csv_data)
         csvfile.close()
+
+
+def bibliography_pairings(page_soup):
+    rows = page_soup.tbody
+    count_data = 0
+    bibliography_dict = {}
+    variable1 = ""                 #variables to store the informmation of the data in the rows
+    variable2 = ""
+    for row in rows.children:            #gets the child tag of table row
+        if row != "\n":
+            for table_data in row.children:     #gets the child tag of table data
+                if table_data != "\n":
+                    for p in table_data.children: #gets the information/tags in the tag p
+                        if p != "\n":
+                            if count_data == 0:
+                                variable1 = p.string #if the count is 0 then retrieve the information and put it in variable 1
+                                count_data += 1
+                            elif count_data != 0: #if the count is not 0 then retrieve the information and put it in variable 2
+                                variable2 = p.string
+                                count_data += 1
+        if count_data == 2:                     #if count is 2 then reset the count
+            count_data = 0
+            bibliography_dict[variable1] = variable2  #store variables into the dictionary with key and value
+            variable1 = ""
+            variable2 = ""
+    print(bibliography_dict)
 
 
 def main():
     # path = input("Please enter the file path to the directory where the html files are stored: ")
     path = "/home/schmidtt/PycharmProjects/PMSS_Scraper/html/"
-    f = open(path + "EVELYN K. WELLS - PINE MOUNTAIN SETTLEMENT SCHOOL COLLECTIONS.html")
-    web_page = BeautifulSoup(f, 'html.parser')
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    file = "EVELYN K. WELLS - PINE MOUNTAIN SETTLEMENT SCHOOL COLLECTIONS.html"
     pmss_images = {}
-    for image in web_page.find_all('img'):
-        temp = Image()
-        for attribute in str(image).split():
+    for file in onlyfiles:
+        f = open(path + file)
+        web_page = BeautifulSoup(f, 'html.parser')
+        pmss_images[file] = image_info(web_page)
+        captions = find_captions(web_page)
+        image_caption_linking(captions, pmss_images[file])
+    for file in onlyfiles:
+        for image in pmss_images[file].keys():
+            print(image + ": ")
+            pmss_images[file][image].list_images()
+            print()
 
-            # this is for pulling out the id that will be used for linking images and captions
-            caption_link(attribute, temp)
+    # write_csv(pmss_images)
+    bibliography_pairings(web_page)
 
-            # this is to pull the filename out of the html
-            image_file_path_info(attribute, temp)
-
-            image_resolution(attribute, temp)
-
-            # Using the temporary variable's information to copy over to a proper variable
-        if temp.file_name != "cropped.jpg":
-            pmss_images[temp.file_name] = copy.copy(temp)
-    for image in pmss_images.keys():
-        print(image + ": ")
-        pmss_images[image].list_images()
-        print()
-
-    write_csv(pmss_images)
 
 
 if __name__ == "__main__":
