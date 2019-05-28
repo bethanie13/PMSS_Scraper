3d92251c46ec76abd4f11545a1ae203cc7bb7519
Tradd Schmidt <37428707+Tradd-Schmidt@users.noreply.github.com>
2019-05-28 17:27:34 -0400

    Changed strip_resolution
    Changed how files are stripped of their resolution information

--- Image.py
+++ Image.py
@@ -24,21 +24,15 @@ class Image:
         :param self: An Image object
         :return: None
         """
-        resolution = str(self.image_resized_resolution[0]) + "x" + str(self.image_resized_resolution[1])
-
-        if self.file_name.count(resolution) == 0:
-            return
-        else:
-
-            final_file_pieces = []
-            ext = self.file_name[-4:]  # Assuming the extension is 3 characters long save the last few characters
-            if "." not in ext:  # If the "." is not in the extension, the extension is 4 characters long
-                ext = self.file_name[-5:]
-            self.file_name = self.file_name[:-len(ext)]
-            split_name = self.file_name.split("-")
-            for piece in split_name:
-                if piece != resolution:
-                    final_file_pieces.append(piece)
+        final_file_pieces = []
+        ext = self.file_name[-4:]  # Assuming the extension is 3 characters long save the last few characters
+        if "." not in ext:  # If the "." is not in the extension, the extension is 4 characters long
+            ext = self.file_name[-5:]
+        self.file_name = self.file_name[:-len(ext)]
+        split_name = self.file_name.split("-")
+        for i in range(len(split_name)-1):
+            print(split_name[i])
+            final_file_pieces.append(split_name[i])
         final_file = ""
         for i in range(len(final_file_pieces)):
             if i == len(final_file_pieces)-1:
@@ -60,3 +54,4 @@ class Image:
         print("Resolution: " + str(self.image_resized_resolution[0]) + "x" + str(self.image_resized_resolution[1]))
         if self.caption != "":
             print("Caption: " + self.caption)
+
