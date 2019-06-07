3c0d2ed61826c02dbb765430d509cbfb70a7ac0c
bethanie13 <williamsbe@berea.edu>
2019-06-07 09:15:55 -0400

    Updated a lot of functions to add more functionality

--- Page.py
+++ Page.py
@@ -3,3 +3,8 @@ class Page:
         self.images = {}  # A dictionary to contain the images from the web page
         self.bibliography = {}  # A dictionary to contain information in the bibiliography
         self.html = ""
+
+    def view_bibliography(self):
+        if self.bibliography:
+            for row_title in self.bibliography:
+                print(f"{row_title}: {self.bibliography[row_title]}")
